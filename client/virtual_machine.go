1b00d251e71e91e931ea4a86bd777369df9fe73d
nikolay-spectro <nikolay@spectrocloud.com>
2023-02-28 12:47:39 -0800

    PLT-333: VM management support.

---
+++ client/virtual_machine.go
@@ -0,0 +1,90 @@
+package client
+
+// remove models after refactoring to hapi models.
+type VirtualMachine struct {
+	APIVersion string   `json:"apiVersion"`
+	APIGroup   string   `json:"apiGroup"`
+	Kind       string   `json:"kind"`
+	Metadata   Metadata `json:"metadata"`
+	Spec       Spec     `json:"spec"`
+}
+
+type Metadata struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+}
+type Spec struct {
+	Running      bool         `json:"running"`
+	SpecTemplate SpecTemplate `json:"template"`
+}
+
+type SpecTemplate struct {
+	Domain   Domain    `json:"domain"`
+	Networks []Network `json:"networks"`
+	Volumes  []Volume  `json:"volumes"`
+}
+
+type Domain struct {
+	CPU       CPU       `json:"cpu"`
+	Devices   Devices   `json:"devices"`
+	Machine   Machine   `json:"machine"`
+	Resources Resources `json:"resources"`
+}
+
+type CPU struct {
+	Cores int `json:"cores"`
+}
+
+type Devices struct {
+	Disks      []Disk      `json:"disks"`
+	Interfaces []Interface `json:"interfaces"`
+}
+
+type Disk struct {
+	Name     string   `json:"name"`
+	DiskType DiskType `json:"disk"`
+}
+
+type DiskType struct {
+	Bus string `json:"bus"`
+}
+
+type Interface struct {
+	Masquerade Masquerade `json:"masquerade"`
+	Name       string     `json:"name"`
+}
+
+type Masquerade struct{}
+
+type Machine struct {
+	Type string `json:"type"`
+}
+
+type Resources struct {
+	Requests Requests `json:"requests"`
+}
+
+type Requests struct {
+	Memory string `json:"memory"`
+}
+
+type Network struct {
+	Name string `json:"name"`
+	Pod  Pod    `json:"pod"`
+}
+
+type Pod struct{}
+
+type Volume struct {
+	Name             string           `json:"name"`
+	ContainerDisk    ContainerDisk    `json:"containerDisk"`
+	CloudInitNoCloud CloudInitNoCloud `json:"cloudInitNoCloud"`
+}
+
+type ContainerDisk struct {
+	Image string `json:"image"`
+}
+
+type CloudInitNoCloud struct {
+	UserData string `json:"userData"`
+}
