0f6cdbc4437ec224d6ee42e6337694db5a1ae82b
Tyler Gillson <tyler.gillson@gmail.com>
2023-05-23 10:31:36 -0700

    do not check loginBanner.Error()

--- client/organization.go
+++ client/organization.go
@@ -15,10 +15,6 @@ func (h *V1Client) PrintLoginBanner(orgName string) error {
 			return fmt.Errorf("invalid Organization: %s", orgName)
 		}
 		return err
-		// TODO: talk to Hubble team. Getting the following on success:
-		// "[GET /v1/auth/org/{orgName}/loginBanner][200] v1AuthOrgLoginBannerGetOK  &{Message:Hello! Welcome to Tyler's dev tenant :-) IsEnabled:true}"
-		// } else if loginBanner.Error() != "" {
-		// 	return errors.New(loginBanner.Error())
 	} else if !loginBanner.Payload.IsEnabled {
 		return nil
 	}
