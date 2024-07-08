3572fa9d187b4c004cbeb7d08ac1776d7b6a01c6
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-07-08 21:39:17 +0530

    fix12

--- client/cluster.go
+++ client/cluster.go
@@ -294,19 +294,11 @@ func (h *V1Client) DownloadLogs(uid string, logFetcherUid string) (io.Writer, er
 
 func (h *V1Client) UpdatePauseAgentUpgradeSettingCluster(upgradeSetting *models.V1ClusterUpgradeSettingsEntity, clusterUID string, context string) error {
 
-	//var params *V1Client.V1SpectroClustersUIDUpgradeSettingsParams
-	// params := clientV1.NewV1SpectroClustersUIDUpgradeSettingsParamsWithContext(h.Ctx)
-	//params := clientV1.NewV1ClusterUpgrad
-	////switch context {
-	////case "project":
-	////	params = clientV1.NewV1SpectroClustersUIDUpgradeSettingsParamsWithContext(h.Ctx)
-	////case "tenant":
-	////	params = clientV1.NewV1SpectroClustersUIDUpgradeSettingsParams()
-	////default:
-	////	return fmt.Errorf("invalid context: %s", context)
-	////}
+	//params := clientV1.NewV1SpectroClustersUIDUpgradesPutParamsWithContext(h.ctx)
+	//params := clientV1.NewV1SpectroClustersUIDUpgradesPutParams
 	//params = params.WithUID(clusterUID).WithBody(upgradeSetting)
-	//_, err := h.GetClusterClient().V1SpectroClustersUIDUpgradeSettings(params)
+	//_, err := h.Client.V1SpectroClustersUIDUpgradesPut()
+	//
 	//if err != nil {
 	//	return err
 	//}
--- client/cluster_profile_import.go
+++ client/cluster_profile_import.go
@@ -1,9 +1,11 @@
 package client
 
 import (
+	"errors"
 	"github.com/go-openapi/runtime"
-
+	"github.com/spectrocloud/palette-api-go/apiutil/transport"
 	clientV1 "github.com/spectrocloud/palette-api-go/client/v1"
+	"github.com/spectrocloud/palette-api-go/models"
 	"github.com/spectrocloud/palette-sdk-go/client/apiutil"
 )
 
@@ -16,4 +18,18 @@ func (h *V1Client) CreateClusterProfileImport(importFile runtime.NamedReadCloser
 		return "", err
 	}
 	return *resp.Payload.UID, nil
+}
+
+func (h *V1Client) ClusterProfileExport(uid string) (*models.V1ClusterProfile, error) {
+	// no need to switch request context here as /v1/clusterprofiles/{uid} works for profile in any scope.
+	params := clientV1.NewV1ClusterProfilesGetParamsWithContext(h.ctx).WithUID(uid)
+	success, err := h.Client.V1ClusterProfilesGet(params)
+	var e *transport.TransportError
+	if errors.As(err, &e) && e.HttpCode == 404 {
+		return nil, nil
+	} else if err != nil {
+		return nil, err
+	}
+
+	return success.Payload, nil
 }
