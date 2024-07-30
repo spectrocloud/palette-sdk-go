6e530909265ac23ff408b217a69a532e53e43d43
Sivaanand Murugesan <sivaanand.m@gmail.com>
2024-07-30 11:20:16 +0530

    removed lint issue

--- client/cluster_profile_import.go
+++ client/cluster_profile_import.go
@@ -1,9 +1,7 @@
 package client
 
 import (
-	"errors"
 	"github.com/go-openapi/runtime"
-	"github.com/spectrocloud/palette-api-go/apiutil/transport"
 	"github.com/spectrocloud/palette-api-go/models"
 
 	clientv1 "github.com/spectrocloud/palette-api-go/client/v1"
@@ -27,12 +25,9 @@ func (h *V1Client) ClusterProfileExport(uid string) (*models.V1ClusterProfile, e
 	// no need to switch request context here as /v1/clusterprofiles/{uid} works for profile in any scope.
 	params := clientv1.NewV1ClusterProfilesGetParamsWithContext(h.ctx).WithUID(uid)
 	success, err := h.Client.V1ClusterProfilesGet(params)
-	var e *transport.TransportError
-	if errors.As(err, &e) && e.HttpCode == 404 {
-		return nil, nil
-	} else if err != nil {
+	if err != nil {
 		return nil, err
 	}
-
 	return success.Payload, nil
+
 }
