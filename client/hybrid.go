254f0d419375e172707e3f618d604d1104015002
Ahmad Ibrahim <ahmad.ibrahim@spectrocloud.com>
2024-11-18 17:10:20 -0800

    feat: implement PutHybridConfig function

--- client/hybrid.go
+++ client/hybrid.go
@@ -15,3 +15,13 @@ func (h *V1Client) EnableClusterHybridSettings(uid string) error {
 	_, err := h.Client.V1SpectroClustersUIDHybridSettings(params)
 	return err
 }
+
+// PutHybridConfig updates the hybrid config with UID configUID for a cluster.
+func (h *V1Client) PutHybridConfig(configUID string, hybridConfig *models.V1AwsCloudHybridConfigEntity) error {
+	params := clientv1.NewV1AwsCloudConfigsUIDHybridConfigParamsWithContext(h.ctx).
+		WithBody(hybridConfig).
+		WithConfigUID(configUID)
+
+	_, err := h.Client.V1AwsCloudConfigsUIDHybridConfig(params)
+	return err
+}
