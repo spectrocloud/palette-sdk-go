d172bf07f321c7667a457ef7628fa66320bf4f18
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-01-28 23:36:23 +0530

    added support for update cluster profile vairiable in running cluster

--- client/cluster.go
+++ client/cluster.go
@@ -361,8 +361,8 @@ func (h *V1Client) GetTheKubernetesCerts(clusterUID string) (*models.V1MachineCe
 }
 
 // UpdateClusterProfileVariableInCluster Update cluster profile variable in running cluster.
-func (h *V1Client) UpdateClusterProfileVariableInCluster(clusterUID string) error {
-	params := clientv1.NewV1SpectroClustersUIDVariablesPatchParamsWithContext(h.ctx).WithUID(clusterUID)
+func (h *V1Client) UpdateClusterProfileVariableInCluster(clusterUID string, body []*models.V1SpectroClusterVariableUpdateEntity) error {
+	params := clientv1.NewV1SpectroClustersUIDVariablesPatchParamsWithContext(h.ctx).WithUID(clusterUID).WithBody(body)
 	_, err := h.Client.V1SpectroClustersUIDVariablesPatch(params)
 	if err != nil {
 		return err
