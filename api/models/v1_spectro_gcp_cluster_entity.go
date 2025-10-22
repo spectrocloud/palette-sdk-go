6edbe929303c54c9a35773e2fd152091db33d285
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-10-22 20:13:38 +0530

    Ps week 43 (#177)
    * Added support for get helm registry sync status
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>
    
    * refreshed hapi
    
    ---------
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>

--- api/models/v1_spectro_gcp_cluster_entity.go
+++ api/models/v1_spectro_gcp_cluster_entity.go
@@ -177,13 +177,16 @@ type V1SpectroGcpClusterEntitySpec struct {
 	// General cluster configuration like health, patching settings, namespace resource allocation, rbac
 	ClusterConfig *V1ClusterConfigEntity `json:"clusterConfig,omitempty"`
 
+	// cluster template
+	ClusterTemplate *V1ClusterTemplateRef `json:"clusterTemplate,omitempty"`
+
 	// machinepoolconfig
 	Machinepoolconfig []*V1GcpMachinePoolConfigEntity `json:"machinepoolconfig"`
 
 	// policies
 	Policies *V1SpectroClusterPolicies `json:"policies,omitempty"`
 
-	// profiles
+	// Cluster profile references that specify which profiles to apply to the cluster. Supports overriding pack values and variables. When template references are used, pack values cannot be overridden as they are retrieved from the template's pre-configured definitions.
 	Profiles []*V1SpectroClusterProfileEntity `json:"profiles"`
 }
 
@@ -200,6 +203,10 @@ func (m *V1SpectroGcpClusterEntitySpec) Validate(formats strfmt.Registry) error 
 	}
 
 	if err := m.validateClusterConfig(formats); err != nil {
+		res = append(res, err)
+	}
+
+	if err := m.validateClusterTemplate(formats); err != nil {
 		res = append(res, err)
 	}
 
@@ -261,6 +268,25 @@ func (m *V1SpectroGcpClusterEntitySpec) validateClusterConfig(formats strfmt.Reg
 				return ve.ValidateName("spec" + "." + "clusterConfig")
 			} else if ce, ok := err.(*errors.CompositeError); ok {
 				return ce.ValidateName("spec" + "." + "clusterConfig")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *V1SpectroGcpClusterEntitySpec) validateClusterTemplate(formats strfmt.Registry) error {
+	if swag.IsZero(m.ClusterTemplate) { // not required
+		return nil
+	}
+
+	if m.ClusterTemplate != nil {
+		if err := m.ClusterTemplate.Validate(formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("spec" + "." + "clusterTemplate")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("spec" + "." + "clusterTemplate")
 			}
 			return err
 		}
@@ -352,6 +378,10 @@ func (m *V1SpectroGcpClusterEntitySpec) ContextValidate(ctx context.Context, for
 		res = append(res, err)
 	}
 
+	if err := m.contextValidateClusterTemplate(ctx, formats); err != nil {
+		res = append(res, err)
+	}
+
 	if err := m.contextValidateMachinepoolconfig(ctx, formats); err != nil {
 		res = append(res, err)
 	}
@@ -400,6 +430,27 @@ func (m *V1SpectroGcpClusterEntitySpec) contextValidateClusterConfig(ctx context
 				return ve.ValidateName("spec" + "." + "clusterConfig")
 			} else if ce, ok := err.(*errors.CompositeError); ok {
 				return ce.ValidateName("spec" + "." + "clusterConfig")
+			}
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (m *V1SpectroGcpClusterEntitySpec) contextValidateClusterTemplate(ctx context.Context, formats strfmt.Registry) error {
+
+	if m.ClusterTemplate != nil {
+
+		if swag.IsZero(m.ClusterTemplate) { // not required
+			return nil
+		}
+
+		if err := m.ClusterTemplate.ContextValidate(ctx, formats); err != nil {
+			if ve, ok := err.(*errors.Validation); ok {
+				return ve.ValidateName("spec" + "." + "clusterTemplate")
+			} else if ce, ok := err.(*errors.CompositeError); ok {
+				return ce.ValidateName("spec" + "." + "clusterTemplate")
 			}
 			return err
 		}
