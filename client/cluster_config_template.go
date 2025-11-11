12ee8e2f5550854c16e2b6d4d7d1be1800571179
Sivaanand Murugesan <sivaanand.m@gmail.com>
2025-11-11 10:48:54 +0530

    fix: add support for cluster template nad policy

---
+++ client/cluster_config_template.go
@@ -0,0 +1,73 @@
+package client
+
+import (
+	"fmt"
+
+	clientv1 "github.com/spectrocloud/palette-sdk-go/api/client/version1"
+	"github.com/spectrocloud/palette-sdk-go/api/models"
+	"github.com/spectrocloud/palette-sdk-go/client/apiutil"
+)
+
+// CreateClusterConfigTemplate creates a new cluster config template.
+func (h *V1Client) CreateClusterConfigTemplate(body *models.V1ClusterTemplateEntity) (*models.V1UID, error) {
+	params := clientv1.NewV1ClusterTemplatesCreateParams().
+		WithBody(body)
+	resp, err := h.Client.V1ClusterTemplatesCreate(params)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Payload, nil
+}
+
+// UpdateClusterConfigTemplate updates an existing cluster config template metadata.
+func (h *V1Client) UpdateClusterConfigTemplate(uid string, body *models.V1ObjectMetaInputEntitySchema) error {
+	params := clientv1.NewV1ClusterTemplatesUIDMetadataUpdateParams().
+		WithUID(uid).
+		WithBody(body)
+	_, err := h.Client.V1ClusterTemplatesUIDMetadataUpdate(params)
+	return err
+}
+
+// GetClusterConfigTemplate retrieves an existing cluster config template by UID.
+func (h *V1Client) GetClusterConfigTemplate(uid string) (*models.V1ClusterTemplate, error) {
+	params := clientv1.NewV1ClusterTemplatesUIDGetParams().
+		WithUID(uid)
+	resp, err := h.Client.V1ClusterTemplatesUIDGet(params)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Payload, nil
+}
+
+// ListClusterConfigTemplates retrieves all cluster config templates.
+func (h *V1Client) ListClusterConfigTemplates() (*models.V1ClusterTemplatesSummary, error) {
+	params := clientv1.NewV1ClusterTemplatesFilterSummaryParams().
+		WithLimit(apiutil.Ptr(int64(0)))
+	resp, err := h.Client.V1ClusterTemplatesFilterSummary(params)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Payload, nil
+}
+
+// GetClusterConfigTemplateByName retrieves an existing cluster config template by name.
+func (h *V1Client) GetClusterConfigTemplateByName(name string) (*models.V1ClusterTemplateSummary, error) {
+	templates, err := h.ListClusterConfigTemplates()
+	if err != nil {
+		return nil, err
+	}
+	for _, template := range templates.Items {
+		if template.Metadata.Name == name {
+			return template, nil
+		}
+	}
+	return nil, fmt.Errorf("cluster config template not found for name %s", name)
+}
+
+// DeleteClusterConfigTemplate deletes an existing cluster config template.
+func (h *V1Client) DeleteClusterConfigTemplate(uid string) error {
+	params := clientv1.NewV1ClusterTemplatesUIDDeleteParams().
+		WithUID(uid)
+	_, err := h.Client.V1ClusterTemplatesUIDDelete(params)
+	return err
+}
