f1daab7117bbc75363138b894e0166a5c8a45ec3
Manimaran <manimaran.avudayappan@spectrocloud.com>
2026-02-16 18:40:35 +0530

    chore: Adding the Get call for EcrRegistrySyncStatus

--- client/registry.go
+++ client/registry.go
@@ -227,6 +227,17 @@ func (h *V1Client) GetOciBasicRegistrySyncStatus(uid string) (*models.V1Registry
 	return resp.Payload, nil
 }
 
+// GetOciEcrRegistrySyncStatus retrieves the sync status of an OCI ECR registry by UID.
+func (h *V1Client) GetOciEcrRegistrySyncStatus(uid string) (*models.V1RegistrySyncStatus, error) {
+	params := clientv1.NewV1EcrRegistriesUIDSyncStatusParamsWithContext(h.ctx).
+		WithUID(uid)
+	resp, err := h.Client.V1EcrRegistriesUIDSyncStatus(params)
+	if err != nil {
+		return nil, err
+	}
+	return resp.Payload, nil
+}
+
 // GetOciEcrRegistry retrieves an existing ECR OCI registry by UID.
 func (h *V1Client) GetOciEcrRegistry(uid string) (*models.V1EcrRegistry, error) {
 	params := clientv1.NewV1EcrRegistriesUIDGetParamsWithContext(h.ctx).
