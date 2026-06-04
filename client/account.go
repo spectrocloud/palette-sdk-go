d85eda1013492406d3ceb5fbe5e3368250acd5d8
Tommy Carver <52047620+tommymcarver@users.noreply.github.com>
2026-06-04 09:31:46 -0700

    Update account.go
    Co-authored-by: Artur Shad Nik <37195151+arturshadnik@users.noreply.github.com>

--- client/account.go
+++ client/account.go
@@ -93,7 +93,7 @@ func (h *V1Client) GetCloudAccountAzureByName(name, scope string) (*models.V1Azu
 			return account, nil
 		}
 	}
-	return nil, fmt.Errorf("azure cloud account with name %q not found", name)
+	return nil, fmt.Errorf("Azure cloud account with name %q not found", name) //nolint:staticcheck
 }
 
 // GetCloudAccountApacheCloudStackByName retrieves an Apache CloudStack cloud account by name (and optional scope) using the list GET.
