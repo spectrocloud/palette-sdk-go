9b67c7a8112e2eaa1ff51f0312841c6139781ad7
Sivaanand Murugesan <sivaanand.m@gmail.com>
2026-06-09 14:00:47 +0530

    PLT-2238: SDK WithTransportDebug() writes the Palette API key to logs in plaintext (#211)
    * chore: initial draft
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>
    
    * Chore: Added cloud config update support for aws and aks
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>
    
    * fix: Improved sdk logs for sensitive infos
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>
    
    * chore: refresh depencies
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>
    
    * chore: draft
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>
    
    * chore: precomit fix
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>
    
    ---------
    
    Signed-off-by: Sivaanand Murugesan <sivaanand.m@gmail.com>

---
+++ client/client_test.go
@@ -0,0 +1,88 @@
+package client
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const testCanaryAPIKey = "canary-secret-api-key-do-not-log-12345"
+
+func captureStderr(t *testing.T, fn func()) string {
+	t.Helper()
+
+	old := os.Stderr
+	r, w, err := os.Pipe()
+	require.NoError(t, err)
+	os.Stderr = w
+
+	done := make(chan string)
+	go func() {
+		var buf bytes.Buffer
+		_, _ = io.Copy(&buf, r)
+		done <- buf.String()
+	}()
+
+	fn()
+	require.NoError(t, w.Close())
+	os.Stderr = old
+	return <-done
+}
+
+func newTestServer(t *testing.T) *httptest.Server {
+	t.Helper()
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, _ *http.Request) {
+		rw.Header().Set("Content-Type", "application/json")
+		rw.WriteHeader(http.StatusOK)
+		_, _ = rw.Write([]byte(`{}`))
+	}))
+}
+
+func assertDebugLogsExcludeCanary(t *testing.T, output string) {
+	t.Helper()
+	assert.NotContains(t, strings.ToLower(output), strings.ToLower(testCanaryAPIKey),
+		"API key must not appear in transport debug logs")
+}
+
+func TestWithTransportDebugAtNewRedactsAPIKey(t *testing.T) {
+	server := newTestServer(t)
+	defer server.Close()
+
+	output := captureStderr(t, func() {
+		c := New(
+			WithPaletteURI(strings.TrimPrefix(server.URL, "http://")),
+			WithAPIKey(testCanaryAPIKey),
+			WithTransportDebug(),
+			WithSchemes([]string{"http"}),
+		)
+		_, err := c.Client.V1ProjectsMetadata(nil)
+		require.NoError(t, err)
+	})
+
+	assertDebugLogsExcludeCanary(t, output)
+}
+
+func TestWithTransportDebugAfterNewRedactsAPIKey(t *testing.T) {
+	server := newTestServer(t)
+	defer server.Close()
+
+	output := captureStderr(t, func() {
+		c := New(
+			WithPaletteURI(strings.TrimPrefix(server.URL, "http://")),
+			WithAPIKey(testCanaryAPIKey),
+			WithSchemes([]string{"http"}),
+		)
+		WithTransportDebug()(c)
+		_, err := c.Client.V1ProjectsMetadata(nil)
+		require.NoError(t, err)
+	})
+
+	assertDebugLogsExcludeCanary(t, output)
+}
